0d40e974933fed7dcd2aaab7f6631739924ff762
simikh <stacy.kolyshko@gmail.com>
2021-08-23 22:38:25 +0300

    tasks results
    * updating 33 files

--- tasks/hard/bubble_sort.py
+++ tasks/hard/bubble_sort.py
@@ -29,7 +29,14 @@
 
 
 def bubble_sort(array: list) -> list:
-    return None
+    run = True
+    while run:
+        run = False
+        for index in range(len(array) - 1):
+            if array[index] > array[index + 1]:
+                array[index], array[index + 1] = array[index + 1], array[index]
+                run = True
+    return array
 
 
 if __name__ == '__main__':
--- tasks/hard/fibonacci.py
+++ tasks/hard/fibonacci.py
@@ -26,7 +26,15 @@
 
 
 def fibonacci(n: int) -> int:
-    return None
+    if n < 0:
+        raise ValueError("n must be higher than 0")
+    elif n == 1 or n == 2:
+        return 1
+    first = 1
+    second = 1
+    for _ in range(n - 2):
+        first, second = second, first + second
+    return second
 
 
 if __name__ == '__main__':
